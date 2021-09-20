0710ee716241cb70e2009fa7e533461fef5d60a6
khoerulrifki <sayarifki8@gmail.com>
2021-09-20 18:57:04 +0700

    Tugas WP2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Khoerul Rifki<br>";
+        echo "Saya Mahasiswa Universitas Bina Sarana Informatika";
+    }
+}
