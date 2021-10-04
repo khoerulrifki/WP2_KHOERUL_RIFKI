41cd5a10051d3db7067d36bd4716c937ab3db5e2
khoerulrifki <sayarifki8@gmail.com>
2021-10-04 20:08:58 +0700

    Pertemuan 3

---
+++ application/views/View_latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <h3>Halo Kawan ... Yuk kita belajar web programing ... !!<br><br></h3>
+    Nilai 1 = <?= $nilai1; ?> <br><br>
+    Nilai 2 = <?= $nilai2; ?> <br><br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
