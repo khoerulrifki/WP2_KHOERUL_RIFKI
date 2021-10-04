95c2f3870dfdf3d6fcc1a1ee982adb9f49e49887
khoerulrifki <sayarifki8@gmail.com>
2021-10-04 22:03:06 +0700

    Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan     : Khoerul</li>
+ <li>Nama Belakang  : Rifki</li>
+ <li>NIM            : 12200154</li>
+ <li>Kelas          : 12.3B.37</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Tanjung Priok, Jakarta Utara</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Futsal</li>
+ <li>Tenis Meja</li>
+ <li>Bulutangkis</li>
+ </ul>
+</section>
